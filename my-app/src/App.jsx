710d3f2a981a9655bd29b26e1094624a5830a095
Raunak Raj <raunakraj151105@gmail.com>
2025-12-25 21:40:13 +0530

    my-app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <div className="chacha">chacha gandu hai</div>
+        <div className="papa">papa aache hai </div>
+      </div>
+      <h1>hii there</h1>
+    </>
+  )
+}
+
+export default App
